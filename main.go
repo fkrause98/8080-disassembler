b9325704d49187a40ce21834094c69bd5eedce2c
Francisco Krause Arnim <fkrausear@gmail.com>
2022-06-21 01:40:31 -0300

    Working diassembler with bin file

--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"encoding/csv"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"strconv"
-	"io/ioutil"
 )
 
 type instruction struct {
@@ -16,7 +16,7 @@ type instruction struct {
 	function string
 }
 
-var instructions = []instruction{}
+var instructions = make(map[string]instruction)
 
 func read_instructions() {
 	csvFile, err := os.Open("8080.csv")
@@ -31,39 +31,55 @@ func read_instructions() {
 		fmt.Println(err)
 	}
 
-
 	for _, line := range csvLines {
 		opcode := line[0]
 		size, _ := strconv.ParseInt(line[2], 16, 64)
 		mnemonic, flags, function := line[1], line[3], line[4]
-		instructions = append(instructions, instruction{opcode, mnemonic, size, flags, function})
+		instructions[opcode[2:]] = instruction{opcode, mnemonic, size, flags, function}
 	}
 	// Ignore CSV header, a bit hacky, but it'll do for now
-	instructions = instructions[1:]
+	// instructions = instructions[1:]
 }
 
 // codebuffer should hold valid 8080 assembly code.
 // pc is the current offset into the code
 // returns the number of bytes of the op
-func disassemble_8080op(codebuffer []byte, pc int) (int64){
-	var code byte = codebuffer[pc]
-	var instruction instruction =  instructions[code]
-	fmt.Printf("0x%x: %q | %q \n", pc, instruction.mnemonic, instruction.function)
+func disassemble_8080op(codebuffer string, pc int) int64 {
+	var code string = codebuffer[pc : pc+2]
+	var instruction instruction = instructions[code]
+	fmt.Printf("0x%X: %q | %q \n", pc, instruction.mnemonic, instruction.function)
 	return instruction.size
 }
 
-func main() {
-	read_instructions()
-	invaders, err := ioutil.ReadFile("invaders.bin")
+// Takes the invader.bin file
+// and turns it into a string
+func read_rom(path string) string {
+	invaders, err := ioutil.ReadFile(path)
 	if err != nil {
 		fmt.Println(err)
 	}
+	n := 0
+	for _, num := range invaders {
+		if num >= 48 {
+			invaders[n] = num
+			n++
+		}
+	}
+	return string(invaders[:n])
+}
 
-	for indx, num := range invaders {
-		invaders[indx] = (num % 48)
+// Takes a path to the invader.bin file
+// and prints its instructions
+func disassemble_rom(path string) {
+	var invaders_rom string = read_rom(path)
+	program_counter := 0
+	for program_counter < len(invaders_rom)-1 {
+		size := disassemble_8080op(invaders_rom, program_counter)
+		program_counter += (int(size) * 2)
 	}
-	for program_counter, _ := range invaders {
-		size := disassemble_8080op(invaders, program_counter)
-		program_counter += (int(size)*2)
-	}
+
+}
+func main() {
+	read_instructions()
+	disassemble_rom("invaders.bin")
 }
